67433290349e628adf3cbe753369b16ccf27f444
jacob kleiman <jacobakleiman@gmail.com>
2021-07-01 14:21:45 -0700

    changes form handling from html to js

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,12 +1,43 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import Fade from 'react-reveal/Fade';
 import { Container, Form, FormGroup, FormLabel, FormControl } from 'react-bootstrap';
+import { navigate } from 'gatsby';
 import PortfolioContext from '../../context/context';
 import Title from '../Title/Title';
 
 const Contact = () => {
   const { contact } = useContext(PortfolioContext);
   const { cta } = contact;
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [subject, setSubject] = useState('');
+  const [message, setMessage] = useState('');
+
+  function encode(data) {
+    return Object.keys(data)
+      .map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
+      .join('&');
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch('/', {
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': e.target.getAttribute('name'),
+        ...name,
+        ...email,
+        ...subject,
+        ...message,
+      }),
+    })
+      .then(() => navigate('/submit'))
+      .catch((error) => {
+        alert(error);
+        navigate('/404');
+      });
+  };
 
   return (
     <section id="contact">
@@ -19,26 +50,54 @@ const Contact = () => {
             </p>
             <Form
               className="form mb-5"
-              method="POST"
-              action="submit"
+              method="post"
+              name="Contact Form"
               data-netlify-recaptcha="true"
               data-netlify="true"
+              onSubmit={handleSubmit}
             >
               <FormGroup>
                 <FormLabel lg="3">Name</FormLabel>
-                <FormControl size="lg" type="text" name="name" />
+                <FormControl
+                  size="lg"
+                  type="text"
+                  name="name"
+                  onChange={(e) => setName(e.target.value)}
+                  value={name}
+                />
               </FormGroup>
               <FormGroup>
                 <FormLabel>Email</FormLabel>
-                <FormControl size="lg" type="email" name="email" required />
+                <FormControl
+                  size="lg"
+                  type="email"
+                  name="email"
+                  onChange={(e) => setEmail(e.target.value)}
+                  value={email}
+                  required
+                />
               </FormGroup>
               <FormGroup>
                 <FormLabel>Subject</FormLabel>
-                <FormControl size="lg" type="text" name="subject" />
+                <FormControl
+                  size="lg"
+                  type="text"
+                  name="subject"
+                  onChange={(e) => setSubject(e.target.value)}
+                  value={subject}
+                />
               </FormGroup>
               <FormGroup>
                 <FormLabel>Message</FormLabel>
-                <FormControl size="lg" as="textarea" rows={5} name="message" required />
+                <FormControl
+                  size="lg"
+                  as="textarea"
+                  rows={5}
+                  name="message"
+                  onChange={(e) => setMessage(e.target.value)}
+                  value={message}
+                  required
+                />
               </FormGroup>
               <FormGroup>
                 <div data-netlify-recaptcha="true" />
